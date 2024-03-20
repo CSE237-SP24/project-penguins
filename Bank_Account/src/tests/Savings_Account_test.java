00295d2e8d117def7caed30cb19ba047c594dbe9
root <root@DESKTOP-KPG8HF4>
2024-03-20 11:31:14 -0500

    Removed old commit, new files work with supporting tests and documentation, Savings and Checking accounts done

---
+++ Bank_Account/src/tests/Savings_Account_test.java
@@ -0,0 +1,54 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import bank_Account.*;
+
+
+class Savings_Account_test {
+
+SavingsAccount testAccount;
+	
+	@BeforeEach
+	void setUp() {
+		 testAccount = new SavingsAccount(0.10);
+	}
+	
+	@Test
+	void testInterest() {
+		testAccount.deposit(25);
+		testAccount.applyInterest();
+		// 3. Use assertions to verify results
+		assertEquals(25.0*(1+0.10), testAccount.getBalance(), 0.01);
+
+	}
+
+	@Test
+	void testWithdrawlLimit() {
+		
+		testAccount.setWithdraw_Limit(10);
+		assertEquals(testAccount.getWithdraw_Limit(), 10);
+
+	}
+
+	@Test
+	void testOverWithdrawl() {
+		
+		testAccount.setWithdraw_Limit(10); 
+		
+		
+		try {
+			testAccount.withdraw(11);
+			fail();
+		} catch (IllegalArgumentException e) {
+			// We expect this, cannot deposit negative amount
+			assertTrue(true);
+		}
+
+	}
+	
+}
