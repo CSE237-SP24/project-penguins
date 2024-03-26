ff8f95a26479333fb0ba74bf888880e0ffdd4943
dzurgon <blgondzur@gmail.com>
2024-03-25 19:14:03 -0500

    fixing script again

--- script.sh
+++ script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+BASE_DIR="$( cd "$( dirname "$1" )" >/dev/null 2>&1 && pwd )"
+
+cd "$BASE_DIR/Bank_Account/src"
+
 # Compiles Menu.java (main method file) and outside package (user_profile.java)
-javac bank_Account/Menu.java user_information/user_profile.java
+javac bank_Account/*.java user_information/*.java
 
 # Runs main method file
-java bank_Account.Menu
+cd "$BASE_DIR"
+
+# this specifies the classpath, which tells where compiled classes are
+java -cp "$BASE_DIR/Bank_Account/src" bank_Account.Menu
