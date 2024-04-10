46cf176e92a5d04cf24f1b0b2afedb2a1c7ea7a8
dzurgon <blgondzur@gmail.com>
2024-04-10 08:31:18 -0500

    testGetClip and testAudioInputStream

--- Bank_Account/src/tests/menu_tests/Menu_tests.java
+++ Bank_Account/src/tests/menu_tests/Menu_tests.java
@@ -6,9 +6,14 @@ import org.junit.jupiter.api.Test;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 
-
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import bank_Account.Menu;
 import user_information.user_profile;
@@ -21,8 +26,29 @@ public class Menu_tests {
     @BeforeEach
     void setup() {
         testMenu = new Menu();
-        
         testUser = new user_profile();
+        testMenu.songPlayerCycler();
     }
-    
+
+    // Tests getAudioInputStreamFromFile's exception handling
+    @Test
+    void testAudioInputStream() throws UnsupportedAudioFileException, IOException {
+
+        File badFile = new File("script.sh");
+        assertEquals(null, testMenu.getAudioInputStreamFromFile(badFile));
+
+        File noFile = new File("");
+        System.out.println(noFile.getAbsolutePath());
+        assertEquals(null, testMenu.getAudioInputStreamFromFile(noFile));
+        
+    }
+
+    // Tests if clips have different thread allocation
+    @Test
+    void testGetClip() throws LineUnavailableException {
+        Clip audioClip = testMenu.getClipFromFile();
+        assertEquals(false, AudioSystem.getClip().equals(audioClip));
+
+    }
+
 }
